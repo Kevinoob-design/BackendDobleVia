25b231b1e7255a93feeec32e5a2b07bd56c08caf
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-03-08 21:41:04 -0400

    ...

--- server/server.js
+++ server/server.js
@@ -32,12 +32,13 @@ const server = app.listen(process.env.PORT, () => {
 
 var io = require('socket.io')(server);
 
-io.on('connection', (socket) => {
-    console.log('------------------------------alooooooooooooooooooooooooooo');
-    io.on("send_message", (data) => {
-        console.log('----------------Este es el otro alooooooooooo');
-        io.broadcast.emit("receive_message", data)
-    })
+// io.on('connection', (socket) => {
+//     console.log('------------------------------alooooooooooooooooooooooooooo');
+
+// });
+
+io.on('connection', socket => {
+    console.log('Made a connection with: ' + socket.id);
 });
 
 
