4de4d3781e6439e2b46f0f58745b7aa727f96c33
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-03-12 20:16:08 -0400

    Added aditional message for the on connection socket

--- server/controllers/Routes.js
+++ server/controllers/Routes.js
@@ -16,7 +16,8 @@ const server = app.listen(process.env.PORT, () => {
 const io = require('socket.io')(server);
 
 io.on('connection', (socket) => { 
-    console.log('THe socket ID of the device: ' + socket.id);
+    console.log('The socket ID of the device: ' + socket.id);
+    console.log('The client is: ' + socket.client);
 });
 
 app.get('/route', (req, res) => {
