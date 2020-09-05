3cd5feff14c8dd19dd9cdc8bae7f0a88a88058c7
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-09-05 00:32:44 -0400

    Added docker compose

--- server.js
+++ server.js
@@ -11,11 +11,13 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Third party middlewares definitions
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 // Third party Middlewares injections
 app.use(cors());
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
